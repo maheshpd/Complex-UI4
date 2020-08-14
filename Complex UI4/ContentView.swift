a251974c2c202d036b07e4d89d7e453eb30ddb4a
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-08-14 07:26:45 +0530

    Initial Commit

---
+++ Complex UI4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Complex UI4
+//
+//  Created by Mahesh Prasad on 14/08/20.
+//  Copyright © 2020 CreatesApp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
